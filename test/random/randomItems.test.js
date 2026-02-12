1f0f50c7fbe9fbf5f6086c90e3088a4fb31f77a4
Randal <randal.t.meyer@gmail.com>
2026-02-12 10:25:15 -0800

    improved testing for more edge cases

--- test/random/randomItems.test.js
+++ test/random/randomItems.test.js
@@ -5,46 +5,54 @@ describe("random", () => {
 	describe("randomItems", () => {
 
 		const array = [1,2,"a","b",{},new Date(),new Map(),"a"];
+		describe(tagLiterals`randomItems(array = ${array}, count, options)`, () => {
 
-		for (let count = 0; count < array.length + 1; count++) {
-			test(tagLiterals`randomItems(${array}, ${count})`, () => {
-				for (let i = 0; i < 1000; i++) {
-					const items = randomItems(array, count);
-					expect(items.length).toBe(count);
-					items.forEach(item => expect(array.includes(item)).toBe(true));
-				}
-			});
+			for (let count = -1; count < array.length + 5; count++) {
+				const expectedCount = Math.max(count, 0);
 
-			test(tagLiterals`randomItems(${array}, ${count}, ${true})`, () => {
-				for (let i = 0; i < 1000; i++) {
-					const items = randomItems(array, count, true);
-					expect(items.length).toBe(count);
-					items.forEach(item => expect(array.includes(item)).toBe(true));
+				test(tagLiterals`randomItems(array, ${count}).length === ${expectedCount}`, () => {
+					for (let i = 0; i < 1000; i++) {
+						// randomize
+						const items = randomItems(array, count);
+						// check length
+						expect(items.length).toBe(expectedCount);
+						// make sure the items are all from the array
+						expect(items.every(item => array.includes(item))).toBe(true);
+					}
+				});
 
-					// if (count < array.length - 1) {
-					// 	const unique = [...new Set(items)];
-					// 	expect(items.length).toEqual(unique.length);
-					// 	expect(items).toEqual(unique);
-					// }
-				}
-			});
+				// byIndex
+				test(tagLiterals`randomItems(array, ${count}, ${{ unique:"byIndex" }})`, () => {
+					for (let i = 0; i < 1000; i++) {
+						// randomize
+						const items = randomItems(array, count, { unique:"byIndex" });
+						// check length
+						expect(items.length).toBe(Math.min(array.length, expectedCount));
+						// make sure the items are all from the array
+						expect(items.every(item => array.includes(item))).toBe(true);
+					}
+				});
 
-			test(tagLiterals`randomItems(${array}, ${count}, ${{ unique:"byValue" }})`, () => {
-				for (let i = 0; i < 1000; i++) {
-					const uniqueArray = array.filter((o,i,a) => i===a.indexOf(o));
+				test(tagLiterals`randomItems(array, ${count}, ${{ unique:"byValue" }})`, () => {
+					for (let i = 0; i < 1000; i++) {
+						// get unique values
+						const uniqueArray = array.filter((o,i,a) => i===a.indexOf(o));
+						// randomize from original
+						const items = randomItems(array, count, { unique:"byValue" });
+						// the last item is a duplicate and thus we can't select it; making the final count test return one fewer item
+						expect(items.length).toBe(Math.min(expectedCount, uniqueArray.length));
+						// make sure the items are all from the array
+						expect(items.every(item => array.includes(item))).toBe(true);
 
-					const items = randomItems(array, count, { unique:"byValue" });
-					// the last item is a duplicate and thus we can't select it; making the final count test return one fewer item
-					expect(items.length).toBe(Math.min(count, uniqueArray.length));
-					items.forEach(item => expect(array.includes(item)).toBe(true));
+						// ensure the resulting items have no duplicates
+						const unique = items.filter((o,i,a) => i===a.indexOf(o));
+						expect(unique.length).toBe(items.length);
+						expect(unique).toEqual(items);
+					}
+				});
+			}
 
-					// ensure the resulting items have no duplicates
-					const unique = items.filter((o,i,a) => i===a.indexOf(o));
-					expect(unique.length).toBe(items.length);
-					expect(unique).toEqual(items);
-				}
-			});
-		}
+		});
 
 	});
 });
