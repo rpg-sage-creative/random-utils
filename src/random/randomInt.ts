070ab4773281b992bab3febc04236c189cfa5a17
Randal <randal.t.meyer@gmail.com>
2026-02-17 15:30:36 -0800

    fix typo in comments

--- src/random/randomInt.ts
+++ src/random/randomInt.ts
@@ -3,7 +3,7 @@ import { randomInt as _randomInt } from "node:crypto";
 /**
  * Ensures min/max are correct before creating the random int.
  * If min and max are the same, that value is returned instead of trying to randomize.
- * Convencience for `crypto.randomInt(Math.min(min, max), Math.max(min, max) + 1)`
+ * Convenience for `crypto.randomInt(Math.min(min, max), Math.max(min, max) + 1)`
  */
 export function randomInt(min: number, max: number): number {
 	// if they are the same, return it
